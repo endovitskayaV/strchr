9ad85418667fa0aa8341bc702752016b0fbd5d15
Valentina Endovitskaya <valyalark@hotmail.com>
2018-02-22 21:25:02 +0300

    done

--- strchr/strchr.cpp
+++ strchr/strchr.cpp
@@ -1,11 +1,36 @@
-// strchr.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
+#include <string.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+char *my_strchr(char *str, int ch) {
+	while (*str != '\0') {
+		if (*str == ch) return str;
+		str++;
+	}
+	return nullptr;
+}
 
 
-int main()
-{
-    return 0;
+int main(){
+	string inputStr;
+	cout << "Enter str\n";
+	cin >> inputStr;
+	char ch;
+	cout << "Enter symbol to find in str\n";
+	cin >> ch;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	char* str = new char[inputStr.length()];
+	strcpy(str, inputStr.c_str());
+	char*  res = my_strchr(str,ch);
+
+	if (res != nullptr)
+		cout << (*res) << " found in '" << str << "' at " << (int)(res-str) << " position.";
+	else cout << ch << " not found in '" << str << "'.";
+	cin.ignore();
+	return 0;
 }
 
